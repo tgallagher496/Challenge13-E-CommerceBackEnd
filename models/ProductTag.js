17a3b53cc24fa8e92dfdbe5f4a7ccac8691324d2
tgallagher496 <tgallagher496@gmail.com>
2022-08-07 22:51:29 -0400

    modified the models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:{
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id:{
+      type: DataTypes.Integer,
+      references:{
+        model: `Product`,
+        key:`id`,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: `Tag`,
+        key: `id`,
+      },
+    },
   },
   {
     sequelize,
